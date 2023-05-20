ea4cfc36cd90a3edc331e94065ac94decb35b2f2
Aayush Sharma <bhaskarsharma64047@gmail.com>
2023-05-20 22:52:43 +0545

    Update info.py

--- info.py
+++ info.py
@@ -68,7 +68,7 @@ TMP_DOWNLOAD_DIRECTORY = environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://channelbot:channelbot@cluster0.anurlrs.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sharmaaay202:sharmaaay202@cluster0.jyci1hu.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_Files')
 
